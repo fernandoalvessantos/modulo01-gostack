8281be95815166223d538a6ef826017e33b8f520
Fernando Santos <fernandosantos.jr@gmail.com>
2019-11-28 23:43:46 -0300

    incluindo rotas

--- index.js
+++ index.js
@@ -2,11 +2,35 @@ const express = require('express');
 
 const server = express();
 
-const users = ['Fernando', 'Diego', 'Joao'];
+server.use(express.json());
+
+const users = ['Fernando', 'Robson', 'Joao'];
+
+server.get('/users',(req, res) => {
+    return res.json(users);
+})
 
 server.get('/users/:index',(req, res) => {
     //lista de objetos vindo de params
     const { index } = req.params;
-    return res.json({message :`Usuário ${users[index]}`});
+    return res.json(users[index]);
 })
+
+server.put('/users/:index',(req, res) => {
+    const { index } = req.params;
+    const { name } = req.body;
+
+    users[index] = name;
+    return res.json(users);
+});
+
+server.delete('/users/:index',(req, res) => {
+    const { index } = req.params;
+
+    users.splice(index, 1);
+    return res.send();
+});
+
+
+
 server.listen(3000);
