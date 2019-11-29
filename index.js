a81a66e775bf6a6e87cc010c267a9d252334887c
Fernando Santos <fernandosantos.jr@gmail.com>
2019-11-28 23:12:39 -0300

    recuperando parametros

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require('express');
 
 const server = express();
 
-server.get('/teste',(req, res) => {
-    return res.json({message :'Hello World'});
+const users = ['Fernando', 'Diego', 'Joao'];
+
+server.get('/users/:index',(req, res) => {
+    //lista de objetos vindo de params
+    const { index } = req.params;
+    return res.json({message :`Usuário ${users[index]}`});
 })
 server.listen(3000);
